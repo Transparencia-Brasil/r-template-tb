53422c9f32b46a7a256aa85f7b9198e2a1d69a0a
Raul de Sá Durlo <durlo.raul@gmail.com>
2025-08-01 12:30:06 -0300

    Update _post-copy.R
    Melhorias na trasnferencia de tasks/../docs para /docs

--- _post-copy.R
+++ _post-copy.R
@@ -1,20 +1,41 @@
 # Este script só entra em ação após renderizar um arquivo qmd, funciona em conjunto com __ quarto.yml
-# Após rodar um arquivo qmd, este script irá transfere arquivos HTML para a pasta `docs/``
+# Após rodar um arquivo qmd, este script irá transferir arquivos HTML para a pasta `docs/`
 
-out_dir <-
-  Sys.getenv("QUARTO_PROJECT_OUTPUT_DIR") |>
-  fs::path_tidy()
+out_dir <- fs::path_tidy(Sys.getenv("QUARTO_PROJECT_OUTPUT_DIR"))
+# cat("\nout_dir:\n", out_dir, "\n")
 
-out_files <-
-  strsplit(Sys.getenv("QUARTO_PROJECT_OUTPUT_FILES"), "\n")[[1]] |>
-  fs::path_tidy()
+out_files <- fs::path_tidy(strsplit(Sys.getenv("QUARTO_PROJECT_OUTPUT_FILES"), "\n")[[1]])
+cat("\nout_files:\n", paste(out_files, collapse = ", "), "\n")
 
 # "notebook_files" for "notebook.html"
-assets <-
-  out_files |>
+assets <- out_files |>
   grep("\\.html$", x = _, value = TRUE) |>
   gsub("\\.html$", "_files", x = _)
-assets <- assets[fs::dir_exists(assets)]
 
-fs::file_copy(out_files, out_dir, overwrite = TRUE)
-fs::dir_copy(assets, fs::path(out_dir, fs::path_file(assets)), overwrite = TRUE)
+assets <- assets[fs::dir_exists(assets)]
+# cat("\nassets:\n", paste(assets, collapse = ", "), "\n")
+
+dest <- fs::path(out_dir, fs::path_file(assets))
+fs::dir_copy(assets, dest, overwrite = TRUE)
+cat("\nArquivos copiados com sucesso!\n")
+
+# atualiza file-structure.json
+files <- list.files(
+  path = "docs",
+  pattern = "html$",
+  full.names = TRUE,
+  recursive = TRUE
+)
+# cat("\nfiles:\n", paste(files, collapse = ",\n"), "\n")
+
+data.frame(subdir = gsub("^docs\\/", "", dirname(files)), path = gsub("^docs\\/", "", files)) |>
+  dplyr::mutate(subdir = ifelse(subdir == "docs", ".", subdir)) |>
+  dplyr::group_nest(subdir) |>
+  tibble::deframe() |>
+  jsonlite::toJSON(pretty = TRUE) |>
+  writeLines("docs/file-structure.json")
+
+cat("\nArquivo file-structure.json atualizado com sucesso!\n")
+# cat("\ndirname(files)\n", paste(dirname(files), collapse = "\n"), "\n")
+# cat("\nsubdir\n", paste(gsub("^docs\\/", "", dirname(files)), collapse = "\n"), "\n")
+# cat("\npath\n", paste(gsub("^docs\\/", "", files), collapse = "\n"), "\n")
